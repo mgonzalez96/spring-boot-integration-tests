6bebe53f4d312623a82750c0bed87e09af910af1
mgonzalez96 <marianaacevedo1996@gmail.com>
2025-09-05 21:34:37 -0500

    1 - Ejercicios Sergio Punto 3

--- src/main/java/com/example/productos/service/ProductoService.java
+++ src/main/java/com/example/productos/service/ProductoService.java
@@ -22,19 +22,62 @@ public class ProductoService {
         return repository.findAll();
     }
 
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Adiciona validacion de nombre no null, valores no iguales a cero y
+	 *  que producto no exista	
+	 */
     public Producto crear(String nombre, BigDecimal precio, Integer stock) {
-        if (precio.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("El precio no puede ser negativo");
+    	if(nombre == null || nombre.isBlank()) {
+			throw new IllegalArgumentException("El nombre no puede estar vacío");    		
+    	}
+        if (precio.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo o igual a cero");
         }
-        if (stock < 0) {
-            throw new IllegalArgumentException("El stock no puede ser negativo");
+        if (stock <= 0) {
+            throw new IllegalArgumentException("El stock no puede ser negativo o igual a cero");
+        }
+        if(repository.findByNombre(nombre).isPresent()) {
+			throw new IllegalArgumentException("El producto con nombre " + nombre + " ya existe");
         }
         Producto p = new Producto(nombre, precio, stock);
         return repository.save(p);
     }
+    
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Edita los datos de un producto, validacion de nombre no null, valores no iguales a cero y
+	 *  que producto exista	
+	 */
+    public Producto editar(String nombre, BigDecimal precio, Integer stock) {
+    	if(nombre == null || nombre.isBlank()) {
+			throw new IllegalArgumentException("El nombre no puede estar vacío");    		
+    	}
+        if (precio.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo o igual a cero");
+        }
+        if (stock <= 0) {
+            throw new IllegalArgumentException("El stock no puede ser negativo o igual a cero");
+        }
+        if(!repository.findByNombre(nombre).isPresent()) {
+        	Producto p = new Producto(nombre, precio, stock);            
+            return repository.save(p);        	
+        }
+        else {
+        	throw new IllegalArgumentException("El producto con nombre " + nombre + " no existe");
+        }
+    }
 
     public Producto obtenerPorId(Long id) {
         return repository.findById(id).orElseThrow(() -> new NotFoundException("Producto no encontrado: " + id));
+    }
+    
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Obtiene un producto por su nombre
+	 */
+    public Producto obtenerPorNombre(String nombre) {
+        return repository.findByNombre(nombre).orElseThrow(() -> new NotFoundException("Producto no encontrado: " + nombre));
     }
 
     public void eliminar(Long id) {
--- src/test/java/com/example/productos/service/ProductoServiceTest.java
+++ src/test/java/com/example/productos/service/ProductoServiceTest.java
@@ -1,41 +1,94 @@
 package com.example.productos.service;
 
 import com.example.productos.domain.Producto;
-import com.example.productos.repository.ProductoRepository;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 @Transactional
+@TestMethodOrder(OrderAnnotation.class)
 class ProductoServiceTest {
 
     @Autowired
     private ProductoService service;
-    @Autowired
-    private ProductoRepository repository;
 
     @Test
+    @Order(1)	
     void crearYObtenerProducto() {
         Producto creado = service.crear("Monitor", new BigDecimal("599.90"), 3);
         Producto obtenido = service.obtenerPorId(creado.getId());
         assertThat(obtenido.getNombre()).isEqualTo("Monitor");
     }
+    
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Edita los datos de un producto
+	 */
+    @Test
+    @Order(2)	
+    void editarYObtenerProducto() {
+        Producto creado = service.editar("Monitor Curvo", new BigDecimal("895.13"), 45);
+        Producto obtenido = service.obtenerPorId(creado.getId());
+        assertThat(obtenido.getNombre()).isEqualTo("Monitor Curvo");
+    }
 
     @Test
+    @Order(4)	
     void eliminarProductoNoExistenteLanzaExcepcion() {
         assertThatThrownBy(() -> service.eliminar(999L))
                 .isInstanceOf(NotFoundException.class);
     }
 
     @Test
+    @Order(3)	
     void precioNegativoLanzaIllegalArgument() {
         assertThatThrownBy(() -> service.crear("Tablet", new BigDecimal("-1.00"), 1))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Lista todos los productos
+	 */
+    @Test
+    @Order(5)	
+    void listarProductos() {
+    	List<Producto> lista = new ArrayList<>();
+        lista = service.listar();
+        assertFalse(lista.isEmpty(),"Lista de Productos Vacía");
+    }
+    
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Lista el producto por nombre
+	 */
+    @Test
+    @Order(6)	
+    void listaProductoByNombre() {
+    	assertThatThrownBy(() -> service.obtenerPorNombre("Monitor Curvo"))
+        .isInstanceOf(NotFoundException.class);
+    }
+    
+    /**
+	 * @Usuario Sergio
+	 * @Descripcion Valida un producto no existente al editar
+	 */
+    @Test
+    @Order(7)
+    void editarProductoNoExistente() {
+        assertThatThrownBy(() -> service.editar("iphone 15 Pro Max", new BigDecimal("1500.00"), 3))
+            .isInstanceOf(NotFoundException.class);
     }
 }
