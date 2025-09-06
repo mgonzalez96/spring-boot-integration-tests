8571ea106bf72f0cc583d4294e922102f0474c6a
Jhon Fernando Mosquera Henao <jfmosquera@gmail.com>
2025-09-05 23:54:30 -0500

    Punto 4 - Ejercicio JhonF

---
+++ target/test-classes/data.sql
@@ -0,0 +1,4 @@
+-- Assuming you have a 'producto' table with these columns
+INSERT INTO producto (nombre, precio, stock) VALUES ('Laptop Pro X', 15300856, 20);
+INSERT INTO producto (nombre, precio, stock) VALUES ('Monitor Curvo 27', 3500632, 45);
+INSERT INTO producto (nombre, precio, stock) VALUES ('Teclado RGB Mecánico', 320652, 110);
